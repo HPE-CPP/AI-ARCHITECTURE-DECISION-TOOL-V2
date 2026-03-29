3621c0548766766c5637909ad75f0e5971766115
Tanmay Sachan <tanmaysachan0005@gmail.com>
2026-03-29 20:55:16 +0530

    fixed footer

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Hrithik M\Documents\My Projects\AI-ARCHITECTURE-DECISION-TOOL\backend\venv
+set VIRTUAL_ENV=C:\Users\tanma\OneDrive\Desktop\HPE\Updated-Codebase\version2\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
